cf1680ed4c6079f66fcd69c076c3cbf8ac43f678
waqasehsanbutt <waqasehsanbutt@yahoo.com>
2022-09-07 16:48:45 +0500

    Enabled CM4 project for PWMGenerator

--- Projects/PEController/Applications/PWMGenerator/CM7/Core/Src/main.c
+++ Projects/PEController/Applications/PWMGenerator/CM7/Core/Src/main.c
@@ -77,7 +77,7 @@ int main(void)
 	/* USER CODE END Boot_Mode_Sequence_0 */
 
 	/* USER CODE BEGIN Boot_Mode_Sequence_1 */
-	/* Wait until CPU2 boots and enters in stop mode or timeout*//*
+	/* Wait until CPU2 boots and enters in stop mode or timeout*/
 
 	timeout = 0xFFFF;
 	while((__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) != RESET) && (timeout-- > 0));
@@ -114,18 +114,18 @@ int main(void)
 	/* When system initialization is finished, Cortex-M7 will release Cortex-M4 by means of
   HSEM notification */
 	/*HW semaphore Clock enable*/
-	//__HAL_RCC_HSEM_CLK_ENABLE();
+	__HAL_RCC_HSEM_CLK_ENABLE();
 	/*Take HSEM */
-	//HAL_HSEM_FastTake(HSEM_ID_0);
+	HAL_HSEM_FastTake(HSEM_ID_0);
 	/*Release HSEM in order to notify the CPU2(CM4)*/
-	//HAL_HSEM_Release(HSEM_ID_0,0);
+	HAL_HSEM_Release(HSEM_ID_0,0);
 	/* wait until CPU2 wakes up from stop mode */
-	//timeout = 0xFFFF;
-	//while((__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) == RESET) && (timeout-- > 0));
-	//if ( timeout < 0 )
-	//{
-		//Error_Handler();
-	//}
+	timeout = 0xFFFF;
+	while((__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) == RESET) && (timeout-- > 0));
+	if ( timeout < 0 )
+	{
+		Error_Handler();
+	}
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
@@ -133,7 +133,7 @@ int main(void)
 
 	while (1)
 	{
-		//SharedMemory_GetRecentMeasurements(&adcVals);
+		SharedMemory_GetRecentMeasurements(&adcVals);
 		MainControl_Loop();
 		/* USER CODE END WHILE */
 
